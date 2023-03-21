8e153b26123f75a425fc7c6f45ea1249008be0db
Anshu <anshur9608837@gmail.com>
2023-03-21 12:31:47 +0530

    mp online

---
+++ code1.java
@@ -0,0 +1,20 @@
+//package Oopsclass.java;
+
+ class A {
+    public static void main(String[] args) {
+        System.out.println("This is A class ");
+        
+    }
+    
+}
+class B{
+    public static void main(String[] args) {
+        System.out.println("this is B classs ");
+
+    }
+}
+class C {
+    public static void main(String[] args) {
+        System.out.println("this is C class ");
+    }
+}
